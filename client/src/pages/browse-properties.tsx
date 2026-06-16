c9be0005b55cb92e29c04a82e27d108616d18f4c
lili12388 <laithou123@gmail.com>
2026-06-16 19:15:10 +0100

    style: slightly lower hero image to balance text placement

--- client/src/pages/browse-properties.tsx
+++ client/src/pages/browse-properties.tsx
@@ -166,9 +166,9 @@ export default function BrowsePropertiesPage() {
       >
         {/* The Image with Parallax Transform */}
         <div 
-          className="absolute left-0 w-full h-[150%] bg-cover will-change-transform"
+          className="absolute left-0 w-full h-[140%] bg-cover will-change-transform"
           style={{ 
-            top: '-30%',
+            top: '-20%',
             backgroundImage: `url('/2%20(2).jpeg')`,
             backgroundPosition: 'center 100%',
             transform: `translateY(${parallaxTranslateY}px)`
